02863fa47c3fd8c1dd2520197877c0ca1cd8a632
matsuotsumami <n.matsuo1616@gmail.com>
2022-01-16 05:41:28 +0900

    HomeのMainをコンポーネント化

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './Home.module.scss'
+import { pagesPath } from '~/utils/$path'
+import { Content } from '~/api/types'
+import { MicroCMSContentId, MicroCMSDate } from 'microcms-js-sdk'
+
+type Props = {
+  blog: (Content & MicroCMSContentId & MicroCMSDate)[]
+}
+
+const Home: React.VFC<Props> = (props) => {
+  const { blog } = props
+  return (
+    <main className={styles.main}>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={pagesPath.posts._id(blog.id).$url()}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+export default Home
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import type { GetStaticProps, NextPage } from 'next'
-import Link from 'next/link'
 import Head from 'next/head'
 import { client } from '~/libs/client'
 import { Content } from '~/api/types'
 import { MicroCMSContentId, MicroCMSDate } from 'microcms-js-sdk'
-import { pagesPath } from '~/utils/$path'
 import { Header } from '~/components/Header'
-import styles from '~/styles/Home.module.scss'
+import { Home as Main } from '~/components/Home'
 
 type Props = {
   blog: (Content & MicroCMSContentId & MicroCMSDate)[]
@@ -21,17 +19,7 @@ const Home: NextPage<Props> = (props) => {
         <title>otsumami blog</title>
       </Head>
       <Header />
-      <main className={styles.main}>
-        <ul>
-          {blog.map((blog) => (
-            <li key={blog.id}>
-              <Link href={pagesPath.posts._id(blog.id).$url()}>
-                <a>{blog.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <Main blog={props.blog} />
     </>
   )
 }
